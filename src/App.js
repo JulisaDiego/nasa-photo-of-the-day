d62e69ee4870eb745c18af69013f9914a4fd8934
JulisaDiego <julisadiego@yahoo.com>
2020-01-10 00:38:09 -0500

    mvp day two

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
-import "./App.css";
-import CardList from "./CardList";
+import React from "react"
+import "./App.css"
+import CardList from "./CardList"
+import styled from "styled-components"
 
+const Headline = styled.h1`
+color: #160f30;
+font-size: 2.5rem;
+background: -webkit-linear-gradient(#202060, #ff6464);
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;
+`
 function App() {
   return (
     <div className="App">
-     <h1>Nasa Photo Of The Day</h1>
+     <Headline>Nasa Photo Of The Day</Headline>
 
      <CardList/>
     </div>
